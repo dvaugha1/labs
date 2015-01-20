35c94cf19093fa91d1270bc491c1e211bdf3320b
Mike Vaughan <dvaugha1@gmail.com>
2015-01-20 08:32:31 -0500

    TTT Update

--- tictactoe.rb
+++ tictactoe.rb
@@ -14,68 +14,18 @@ class Player
     @playerx = "X"
     @playero = "O"
   end
-def inputx(spaces)
+def self.inputx(spaces)
+  @playerx = "X"
   print "Player X: Please input a number from 1 through 9: "
   input = gets.chomp
-  if input== "1"
-    spaces.map! { |x| x == "1" ? @playerx : x }
-  end
-  if input == "2"
-    spaces.map! { |x| x == "2" ? @playerx : x }
-  end
-  if input == "3"
-    spaces.map! { |x| x == "3" ? @playerx : x }
-  end
-  if input == "4"
-    spaces.map! { |x| x == "4" ? @playerx : x }
-  end
-  if input == "5"
-    spaces.map! { |x| x == "5" ? @playerx : x }
-  end
-  if input == "6"
-    spaces.map! { |x| x == "6" ? @playerx : x }
-  end
-  if input == "7"
-    spaces.map! { |x| x == "7" ? @playerx : x }
-  end
-  if input == "8"
-    spaces.map! { |x| x == "8" ? @playerx : x }
-  end
-  if input == "9"
-    spaces.map! { |x| x == "9" ? @playerx : x }
-  end
+  spaces[input.to_i - 1] = @playerx
 end
 
-def inputo (spaces)
+def self.inputo (spaces)
+  @playero = "O"
   print "Player O: Please input a number from 1 through 9: "
   input = gets.chomp
-  if input == "1"
-    spaces.map! { |x| x == "1" ? @playero : x }
-  end
-  if input == "2"
-    spaces.map! { |x| x == "2" ? @playero : x }
-  end
-  if input == "3"
-    spaces.map! { |x| x == "3" ? @playero : x }
-  end
-  if input == "4"
-    spaces.map! { |x| x == "4" ? @playero : x }
-  end
-  if input == "5"
-    spaces.map! { |x| x == "5" ? @playero : x }
-  end
-  if input == "6"
-    spaces.map! { |x| x == "6" ? @playero : x }
-  end
-  if input == "7"
-    spaces.map! { |x| x == "7" ? @playero : x }
-  end
-  if input == "8"
-    spaces.map! { |x| x == "8" ? @playero : x }
-  end
-  if input == "9"
-    spaces.map! { |x| x == "9" ? @playero : x }
-  end
+  spaces[input.to_i - 1] = @playero
 end
 end
 
@@ -83,35 +33,10 @@ class Comp
   def initialize
   @comp = "O"
   end
-def choose (spaces)
+def self.choose (spaces)
+ @comp = "O"
  input = spaces.sample.to_s
- if input == "1"
-   spaces.map! { |x| x == "1" ? @comp : x }
- end
- if input == "2"
-   spaces.map! { |x| x == "2" ? @comp : x }
- end
- if input == "3"
-   spaces.map! { |x| x == "3" ? @comp : x }
- end
- if input == "4"
-   spaces.map! { |x| x == "4" ? @comp : x }
- end
- if input == "5"
-   spaces.map! { |x| x == "5" ? @comp : x }
- end
- if input == "6"
-   spaces.map! { |x| x == "6" ? @comp : x }
- end
- if input == "7"
-   spaces.map! { |x| x == "7" ? @comp : x }
- end
- if input == "8"
-   spaces.map! { |x| x == "8" ? @comp : x }
- end
- if input == "9"
-   spaces.map! { |x| x == "9" ? @comp : x }
- end
+ spaces[input.to_i - 1] = @comp
 end
 end
 
@@ -121,7 +46,7 @@ class Game
     @tictactoecomp = tictactoecomp
   end
 
-def display(spaces)
+def self.display(spaces)
   system "clear"
   spaces.each_index do |x|
     if (x == 2) || (x == 5)
@@ -132,23 +57,23 @@ def display(spaces)
   end
 end
 
-def win?(spaces)
+def self.win?(spaces)
   WIN.any? do |a, b, c|
-  if spaces[a] == spaces[b] && spaces[b] == spaces[c]
+    if spaces[a] == spaces[b] && spaces[b] == spaces[c]
   return spaces[a]
 end
 end
 end
 
 
-def @tictactoep2p(spaces)
+def self.tictactoep2p(spaces)
   turn_count = 1
-  until win?(spaces) || turn_count == 10 do
-    display(spaces)
+  until Game.win?(spaces) || turn_count == 10 do
+    Game.display(spaces)
     Player.inputx(spaces)
     turn_count += 1
-    display(spaces)
-    if win?(spaces)
+    Game.display(spaces)
+    if Game.win?(spaces)
       puts "X Wins"
     elsif
       turn_count == 10
@@ -156,24 +81,24 @@ def @tictactoep2p(spaces)
     else
       Player.inputo(spaces)
       turn_count +=1
-      if win?(spaces)
+      if Game.win?(spaces)
         puts "O wins"
       end
     end
-    if win?(spaces) == true
+    if Game.win?(spaces) == true
       break
     end
   end
 end
 
-def @tictactoecomp(spaces)
+def self.tictactoecomp(spaces)
   turn_count = 1
-  until win?(spaces) || turn_count == 10 do
+  until Game.win?(spaces) || turn_count == 10 do
     display(spaces)
     Player.inputx(spaces)
     turn_count += 1
     display(spaces)
-    if win?(spaces)
+    if Game.win?(spaces)
       puts "X Wins"
     elsif
       turn_count == 10
@@ -181,11 +106,11 @@ def @tictactoecomp(spaces)
     else
       Comp.choose(spaces)
       turn_count +=1
-      if win?(spaces)
+      if Game.win?(spaces)
         puts "O wins"
       end
     end
-    if win?(spaces) == true
+    if Game.win?(spaces) == true
       break
     end
   end
